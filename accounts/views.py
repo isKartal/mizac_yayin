b9a56a0b7857f68fe2b0fffcccdb302499766299
iskartal <i.berkkartal@gmail.com>
2025-02-20 18:00:57 +0300

    index, about güncellendi profiles app i güncellendi

--- accounts/views.py
+++ accounts/views.py
@@ -35,7 +35,7 @@ def user_login(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('index')
+            return redirect('profiles')
         else:
             messages.error(request, "Kullanıcı adı veya şifre yanlış.")
 
