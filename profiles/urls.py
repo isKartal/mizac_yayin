c7614495b2db16b651e9a717d4697dc90d10d79e
iskartal <i.berkkartal@gmail.com>
2025-03-07 02:57:40 +0300

    mizacım ve önerilerim kısımları oluşturuldu veritabanından doğru sonuçlar çekildi

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('profiles/', views.profiles, name='profiles'),
+    path('my_suggestions/', views.my_suggestions, name='my_suggestions'),
+    path('my_temperament/', views.my_temperament, name='my_temperament'),
 ]
