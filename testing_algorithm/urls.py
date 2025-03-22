10a00218385dd127e86271d0127ceacce4d0ba44
iskartal <i.berkkartal@gmail.com>
2025-03-23 02:48:03 +0300

    test öncesi yeni ekran eklendi

--- testing_algorithm/urls.py
+++ testing_algorithm/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('tests/', views.direct_to_test, name='test_list'),
     path('tests/<int:test_id>/<int:question_index>/', views.take_test, name='take_test'),
     path('results/<int:result_id>/', views.test_result, name='test_result'),
+    path('tests/<int:test_id>/intro/', views.test_intro, name='test_intro'),
 ]
 
 if settings.DEBUG:
