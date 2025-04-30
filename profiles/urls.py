0e17997676387457e02601d64a1092bc96e25d44
iskartal <i.berkkartal@gmail.com>
2025-04-30 12:18:49 +0300

    önerilerim güncelleme vol-2

--- profiles/urls.py
+++ profiles/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('content/<int:content_id>/detail/', views.content_detail, name='content_detail'),
     path('content/<int:content_id>/toggle_like/', views.toggle_like_content, name='toggle_like_content'),
     path('content/<int:content_id>/toggle_save/', views.toggle_save_content, name='toggle_save_content'),
+    path('api/all_contents/', views.api_all_contents, name='api_all_contents'),
 ]
